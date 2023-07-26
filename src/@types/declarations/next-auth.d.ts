0df13a93ca6e12b02a15ab6b03ee806fca761e3c
GLEF1X <glebgar567@gmail.com>
2023-07-26 17:56:25 -0400

    fix `next-auth` type declarations

--- src/@types/declarations/next-auth.d.ts
+++ src/@types/declarations/next-auth.d.ts
@@ -1,15 +1,17 @@
-declare module 'next-auth' {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
-  interface Session {
-    accessToken: string;
-    user?: {
-      name: string;
-      email: string;
-      image: string | null;
-      id: string;
-      bio: string | null;
-    };
+declare global {
+  declare module 'next-auth' {
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+      accessToken: string;
+      user?: {
+        name: string;
+        email: string;
+        image: string | null;
+        id: string;
+        bio: string | null;
+      };
+    }
   }
 }
