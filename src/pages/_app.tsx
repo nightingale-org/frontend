65b17b55ee9b7ca139dc85baa119c23bf7a80f57
GLEF1X <glebgar567@gmail.com>
2023-08-09 23:54:46 -0400

    add `react-query-devtools` to debug queries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,10 @@ import type { AppProps } from 'next/app';
 import { SessionProvider, useSession } from 'next-auth/react';
 import { NextPage } from 'next';
 import { inter } from '@/lib/fonts';
-import { useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from '@/components/ui/toaster';
+import dynamic from 'next/dynamic';
 
 export type NextPageWithLayoutAndAuth<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: React.ReactElement) => React.ReactElement;
@@ -16,11 +17,36 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayoutAndAuth;
 };
 
+const ReactQueryDevtools = dynamic(
+  () =>
+    import('@tanstack/react-query-devtools/build/lib/index.prod.js').then((d) => ({
+      default: d.ReactQueryDevtools
+    })),
+  {
+    ssr: false
+  }
+);
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps }
 }: AppPropsWithLayout) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false
+          }
+        }
+      })
+  );
+  const [showDevtools, setShowDevtools] = useState(false);
+
+  useEffect(() => {
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
+
   const getLayout = Component.getLayout || ((page) => page);
 
   return (
@@ -34,6 +60,11 @@ export default function App({
               getLayout(<Component {...pageProps} />)
             )}
             <Toaster />
+            {showDevtools && (
+              <Suspense fallback={null}>
+                <ReactQueryDevtools />
+              </Suspense>
+            )}
           </main>
         </SessionProvider>
       </Hydrate>
