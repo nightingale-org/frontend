10ae9fb2737e7acedcd492c41b0171b176a28ad7
GLEF1X <glebgar567@gmail.com>
2023-08-12 21:59:13 -0400

    fixed some accessibility issues

--- src/components/sidebar/DesktopSidebar.tsx
+++ src/components/sidebar/DesktopSidebar.tsx
@@ -21,7 +21,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = () => {
       <SettingsModal isOpen={isSettingsModalOpen} onClose={onSettingsModalClose} />
       <div className="hidden justify-between lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:flex lg:w-20 lg:flex-col lg:overflow-y-auto lg:border-r-[1px] lg:bg-white lg:pb-4 xl:px-6">
         <nav className="mt-4 flex flex-col justify-between">
-          <ul role="list" className="flex flex-col items-center space-y-1">
+          <ul className="flex flex-col items-center space-y-1">
             {routes.map((route) => (
               <DesktopItem key={route.label} {...route} />
             ))}
@@ -31,6 +31,8 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = () => {
           <div
             onClick={() => setIsSettingsModalOpen(true)}
             className="cursor-pointer transition hover:opacity-75"
+            role="button"
+            tabIndex={0}
           >
             <Avatar>
               <AvatarImage src={session.user.image ?? '/images/placeholder.jpg'} />
